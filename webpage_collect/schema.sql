efc24dc84b3ed5033936e2e2b50b790d27ec05a9
root <root@DESKTOP-9CM1TEE.localdomain>
2021-02-22 23:14:56 +0100

    ADDED: frontend/backend bulk code

---
+++ webpage_collect/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS keystrokes;
+
+CREATE TABLE keystrokes (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	user_id INTEGER,
+	type BIT,
+	key TEXT,
+	keycode BIGINT,
+	clocktime INTEGER,
+	lastkey INTEGER
+);
---
+++ webpage_collect/view.py
@@ -0,0 +1,41 @@
+#import functools
+import json
+
+from flask import (
+        Blueprint, g, request, render_template, redirect, url_for
+)
+
+from webpage_collect.db import get_db
+
+bp = Blueprint('view', __name__)
+
+@bp.route('/')
+def index():
+    return redirect(url_for('view.input'))
+
+@bp.route('/input', methods=('GET', 'POST'))
+def input():
+    if request.method == 'POST':
+        req = request.get_json()
+        #for x in range(len(req)):
+
+         #   db = get_db()
+          #  db.execute(
+          #          'INSERT INTO keystrokes (user_id, clocktime, time, altkey, key, keycode)'
+          #          ' VALUES (?, ?, ?, ?, ?, ?)',
+          #          (req[x]['user_id'], req[x]['clockTime'], req[x]['timestamp'], req[x]['altkey'], req[x]['key'], req[x]['keyCode'])
+          #  )
+          #  db.commit()
+        db = get_db()
+        db.execute(
+                'INSERT INTO keystrokes (user_id, type, key, keycode, clocktime, lastkey)'
+                ' VALUES (?, ?, ?, ?, ?, ?)',
+                (req['user_id'], req['type'], req['key'], req['keyCode'], req['clockTime'], req['lastkey'])
+        )
+        db.commit()
+    return render_template('input.html')
+
+@bp.route('/textarea', methods=('GET', 'POST'))
+def textarea():
+    return render_template('textarea.html')
+
